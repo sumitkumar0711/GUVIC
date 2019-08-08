d74e97c7b822f9eaf931bc04a0fc5dc9b23b9364
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-08-09 00:42:01 +0530

    Create VerifyAnyArmstrongNumber.c

---
+++ VerifyAnyArmstrongNumber.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int sum = 0,di,i,n,nn1,nn2,counter=0;
+    scanf("%d",&n);
+    nn1=n;
+    for(i=0;i<=(nn1+1);i++)
+    {
+        counter++;
+        nn1=nn1/10;
+    }
+    nn2 = n;
+    for(i=0;(i<=nn2+1);i++)
+    {
+        di = nn2%10;
+        sum = sum + (pow(di,counter));
+        nn2 = nn2/10;
+    }
+    if(sum==n)
+    printf("Armstrong");
+    else
+    printf("Not an armstrong");
+    return 0;
+}
